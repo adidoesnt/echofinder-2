d899f285dcc9c518e9cffd4d4079f33a4c615601
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-04-16 16:00:07 +0100

    add examples command

--- src/echofinder/bot/main.py
+++ src/echofinder/bot/main.py
@@ -20,8 +20,25 @@ def help_handler(message: Message):
     reply: str = f"{config['bot']['handlers']['help']['message']}"
     bot.reply_to(message, reply)
     
+@bot.message_handler(commands=['examples'])
+def examples_handler(message: Message):
+    reply: str = f"{config['bot']['handlers']['examples']['message']}"
+    bot.reply_to(message, reply)
+    
+def set_bot_commands():
+    commands = [
+        telebot.types.BotCommand(command="/start", description=config['bot']['handlers']['start']['description']),
+        telebot.types.BotCommand(command="/help", description=config['bot']['handlers']['help']['description']),
+        telebot.types.BotCommand(command="/examples", description=config['bot']['handlers']['examples']['description']),
+    ]
+    
+    bot.set_my_commands(commands)
+    
 def init_bot():
     print(f"Initialising bot in {ENV} mode...")
+    
+    set_bot_commands()
+    print("Bot commands set")
     
     if ENV == "dev":
         bot.infinity_polling()
